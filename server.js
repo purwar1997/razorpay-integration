f96e47de5b23d9aa53fbfc7112426034ad6709f4
purwar1997 <shubhampurwar511@gmail.com>
2023-03-13 23:03:45 +0530

    added payments API to capture and fetch payments

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post(
     const { amount } = req.body;
 
     if (!amount) {
-      throw new Error('Please provide an order amount');
+      throw new Error('Order amount not provided');
     }
 
     const order = await razorpay.orders.create({
@@ -42,7 +42,7 @@ app.post(
 app.get(
   '/api/orders',
   asyncHandler(async (_req, res) => {
-    const orders = await razorpay.orders.all({ 'expand[]': ['payments', 'payments.card'] });
+    const orders = await razorpay.orders.all({ 'expand[]': 'payments.card' });
 
     res.status(200).json({
       success: true,
@@ -59,7 +59,7 @@ app.get(
     const { orderId } = req.params;
 
     if (!orderId) {
-      throw new Error('Please provide an order id');
+      throw new Error('Order id not provided');
     }
 
     const order = await razorpay.orders.fetch(orderId);
@@ -72,14 +72,14 @@ app.get(
   })
 );
 
-// Fetch all payments for an order
+// Fetch all payments of an order
 app.get(
   '/api/order/:orderId/payments',
   asyncHandler(async (req, res) => {
     const { orderId } = req.params;
 
     if (!orderId) {
-      throw new Error('Please provide an order id');
+      throw new Error('Order id not provided');
     }
 
     const payments = await razorpay.orders.fetchPayments(orderId);
@@ -91,5 +91,55 @@ app.get(
     });
   })
 );
+
+// Capture an authorized payment
+app.post(
+  '/api/payment/capture',
+  asyncHandler(async (req, res) => {
+    const { paymentId, amount } = req.body;
+
+    if (!paymentId) {
+      throw new Error('Payment id not provided');
+    }
+
+    if (!amount) {
+      throw new Error('Payment amount not provided');
+    }
+
+    const payment = await razorpay.payments.capture(paymentId, { amount, currency: 'INR' });
+
+    res.status(201).json({
+      success: true,
+      message: 'Payment successfully captured',
+      payment,
+    });
+  })
+);
+
+// Fetch a payment by id
+app.get(
+  '/api/payment/:paymentId',
+  asyncHandler(async (req, res) => {
+    const { paymentId } = req.params;
+
+    if (!paymentId) {
+      throw new Error('Payment id not provided');
+    }
+
+    const payment = await razorpay.payments.fetch(paymentId, { 'expand[]': 'card' });
+
+    res.status(200).json({
+      success: true,
+      message: 'Payment details successfully fetched',
+      payment,
+    });
+  })
+);
+
+// Fetch all payments
+
+// Fetch card details of a payment
+
+// Refund a payment
 
 app.listen(config.PORT, () => console.log(`Server is running on http://localhost:${config.PORT}`));
