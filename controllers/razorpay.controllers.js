3f38a315ac0f8881b470a9c3346fb3a534938202
purwar1997 <shubhampurwar511@gmail.com>
2023-03-14 15:11:52 +0530

    restructured project into routes and controllers

---
+++ controllers/razorpay.controllers.js
@@ -0,0 +1,228 @@
+const uuid = require('uuid');
+const razorpay = require('./config/razorpay.config');
+const asyncHandler = require('./services/asyncHandler');
+
+// Create an order
+export const createOrder = asyncHandler(async (req, res) => {
+  const { amount } = req.body;
+
+  if (!amount) {
+    throw new Error('Order amount not provided');
+  }
+
+  const order = await razorpay.orders.create({
+    amount: amount * 100,
+    currency: 'INR',
+    receipt: uuid.v4(),
+  });
+
+  res.status(201).json({
+    success: true,
+    message: 'Order successfully created',
+    order,
+  });
+});
+
+// Fetch all orders
+export const fetchAllOrders = asyncHandler(async (_req, res) => {
+  const orders = await razorpay.orders.all({ 'expand[]': 'payments.card' });
+
+  res.status(200).json({
+    success: true,
+    message: 'All orders successfully fetched',
+    orders: orders.items,
+  });
+});
+
+// Fetch an order by id
+export const fetchOrderById = asyncHandler(async (req, res) => {
+  const { orderId } = req.params;
+
+  if (!orderId) {
+    throw new Error('Order id not provided');
+  }
+
+  const order = await razorpay.orders.fetch(orderId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Order successfully fetched',
+    order,
+  });
+});
+
+// Fetch all payments of an order
+export const fetchOrderPayments = asyncHandler(async (req, res) => {
+  const { orderId } = req.params;
+
+  if (!orderId) {
+    throw new Error('Order id not provided');
+  }
+
+  const payments = await razorpay.orders.fetchPayments(orderId);
+
+  res.status(200).json({
+    success: true,
+    message: `Payments successfully fetched for order ${orderId}`,
+    payments,
+  });
+});
+
+// Capture an authorized payment
+export const capturePayment = asyncHandler(async (req, res) => {
+  const { paymentId } = req.params;
+  const { amount } = req.body;
+
+  if (!paymentId) {
+    throw new Error('Payment id not provided');
+  }
+
+  if (!amount) {
+    throw new Error('Payment amount not provided');
+  }
+
+  const payment = await razorpay.payments.capture(paymentId, { amount, currency: 'INR' });
+
+  res.status(201).json({
+    success: true,
+    message: 'Payment successfully captured',
+    payment,
+  });
+});
+
+// Fetch a payment by id
+export const fetchPaymentById = asyncHandler(async (req, res) => {
+  const { paymentId } = req.params;
+
+  if (!paymentId) {
+    throw new Error('Payment id not provided');
+  }
+
+  const payment = await razorpay.payments.fetch(paymentId, { 'expand[]': 'card' });
+
+  res.status(200).json({
+    success: true,
+    message: 'Payment details successfully fetched',
+    payment,
+  });
+});
+
+// Fetch all payments
+export const fetchAllPayments = asyncHandler(async (req, res) => {
+  const payments = await razorpay.payments.all({ 'expand[]': 'card' });
+
+  res.status(200).json({
+    success: true,
+    message: 'Payments successfully fetched',
+    payments: payments.items,
+  });
+});
+
+// Fetch card details of a payment
+export const fetchPaymentCardDetails = asyncHandler(async (req, res) => {
+  const { paymentId } = req.params;
+
+  if (!paymentId) {
+    throw new Error('Payment id not provided');
+  }
+
+  const card = await razorpay.payments.fetchCardDetails(paymentId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Card details successfully fetched',
+    card,
+  });
+});
+
+// Initiate instant refund for a payment (full or partial)
+export const initiatePaymentRefund = asyncHandler(async (req, res) => {
+  const { paymentId } = req.params;
+  const { amount } = req.body;
+
+  if (!paymentId) {
+    throw new Error('Payment id not provided');
+  }
+
+  if (!amount) {
+    throw new Error('Refund amount not provided');
+  }
+
+  const refund = await razorpay.payments.refund(paymentId, {
+    amount: amount * 100,
+    speed: 'optimum',
+    receipt: new Date(),
+  });
+
+  res.status(201).json({
+    success: true,
+    message: 'Refund successfully initiated',
+    refund,
+  });
+});
+
+// Fetch multiple refunds for a payment
+export const fetchMultipleRefundsOfPayment = asyncHandler(async (req, res) => {
+  const { paymentId } = req.params;
+
+  if (!paymentId) {
+    throw new Error('Payment id not provided');
+  }
+
+  const refunds = await razorpay.payments.fetchMultipleRefund(paymentId);
+
+  res.status(201).json({
+    success: true,
+    message: 'Refunds successfully fetched',
+    refunds: refunds.items,
+  });
+});
+
+// Fetch a specific refund for a payment
+export const fetchSpecificRefundOfPayment = asyncHandler(async (req, res) => {
+  const { paymentId, refundId } = req.params;
+
+  if (!paymentId) {
+    throw new Error('Payment id not provided');
+  }
+
+  if (!refundId) {
+    throw new Error('Refund id not provided');
+  }
+
+  const refund = await razorpay.payments.fetchRefund(paymentId, refundId);
+
+  res.status(201).json({
+    success: true,
+    message: 'Refund successfully fetched',
+    refund,
+  });
+});
+
+// Fetch all refunds
+export const fetchAllRefunds = asyncHandler(async (_req, res) => {
+  const refunds = await razorpay.refunds.all({ count: 20 });
+
+  res.status(201).json({
+    success: true,
+    message: 'Refunds successfully fetched',
+    refunds: refunds.items,
+  });
+});
+
+// Fetch refund by id
+export const fetchRefundById = asyncHandler(async (req, res) => {
+  const { refundId } = req.params;
+
+  if (!refundId) {
+    throw new Error('Refund id not provided');
+  }
+
+  const refund = await razorpay.refunds.fetch(refundId);
+
+  res.status(201).json({
+    success: true,
+    message: 'Refund successfully fetched',
+    refund,
+  });
+});
---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const {
+  createOrder,
+  fetchAllOrders,
+  fetchOrderById,
+  fetchOrderPayments,
+  capturePayment,
+  fetchPaymentById,
+  fetchAllPayments,
+  fetchPaymentCardDetails,
+  initiatePaymentRefund,
+  fetchMultipleRefundsOfPayment,
+  fetchSpecificRefundOfPayment,
+  fetchAllRefunds,
+  fetchRefundById,
+} = require('../controllers/razorpay.controllers');
+
+const router = express.Router();
+
+router.post('/api/order/create', createOrder);
+router.get('/api/orders', fetchAllOrders);
+router.get('/api/order/:orderId', fetchOrderById);
+router.get('/api/order/:orderId/payments', fetchOrderPayments);
+router.post('/api/payment/:paymentId/capture', capturePayment);
+router.get('/api/payment/:paymentId', fetchPaymentById);
+router.get('/api/payments', fetchAllPayments);
+router.get('/api/payment/:paymentId/card', fetchPaymentCardDetails);
+router.post('/api/payment/:paymentId/refund', initiatePaymentRefund);
+router.get('/api/payment/:paymentId/refunds', fetchMultipleRefundsOfPayment);
+router.get('/api/payment/:paymentId/refund/:refundId', fetchSpecificRefundOfPayment);
+router.get('/api/refunds', fetchAllRefunds);
+router.get('/api/refund/:refundId', fetchRefundById);
+
+module.exports = router;
